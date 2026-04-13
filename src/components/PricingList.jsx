116fff5228a1447f5f9a348aebf719ac749f2847
Naveen <navinmachari77@gmail.com>
2026-04-13 23:44:26 +0530

    Initial Commit

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -29,7 +29,7 @@ const PricingList = () => {
 
           <Button
             className="w-full mb-6"
-            href={item.price ? "#pricing" : "mailto:naveen2004@gmail.com"}
+            href={item.price ? "#pricing" : "mailto:navinmachari77@gmail.com"}
             white={!!item.price}
           >
             {item.price ? "Get started" : "Contact us"}
